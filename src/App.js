88b9ef5ee5b257c72525f458569e9a54047f51c0
Andrew <dassonville.andrew@gmail.com>
2017-10-31 19:13:16 +0000

    Add firebase support

--- src/App.js
+++ src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
 import RegionSelect from 'react-region-select';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import axios from 'axios';
 import './App.css';
 
-const repository = 'https://raw.githubusercontent.com/SouthEugeneRoboticsTeam/bucket-dataset/master/images/';
+const repository =
+  'https://raw.githubusercontent.com/SouthEugeneRoboticsTeam/bucket-dataset/master/images/';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCNTrciChFb30GbolZPhIe_Hayhohl2DjU',
+  authDomain: 'tagger-9c2dc.firebaseapp.com',
+  projectId: 'tagger-9c2dc'
+});
+
+const db = firebase.firestore();
+const imagesRef = db.collection('images');
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = { regions: [], image: '00001.jpg' };
+    this.state = { regions: [], image: false, images: [] };
 
     this.onChange = this.onChange.bind(this);
     this.regionRenderer = this.regionRenderer.bind(this);
     this.removeRegion = this.removeRegion.bind(this);
+    this.loadNextImage = this.loadNextImage.bind(this);
+  }
+
+  componentWillMount() {
+    document.addEventListener('keydown', this.onKeyPressed.bind(this));
+
+    axios.get(`${repository}_manifest.json`).then(res => {
+      this.setState({ images: res.data });
+      this.loadNextImage();
+    });
+  }
+
+  onKeyPressed({ key }) {
+    if (key === 'Enter') {
+      this.saveRegions(this.state.image, this.state.regions);
+      this.loadNextImage();
+    }
+  }
+
+  saveRegions(image, regions) {
+    const roundedRegions = regions.map(({ x, y, width, height }) => {
+      return {
+        x: Math.max(x, 0),
+        y: Math.max(y, 0),
+        width: Math.min(width, 100),
+        height: Math.min(height, 100)
+      };
+    });
+
+    imagesRef
+      .add({
+        image,
+        regions: roundedRegions
+      })
+      .then(docRef => {
+        console.log(`Successfully wrote to ${docRef.id}`);
+      })
+      .catch(err => {
+        console.log(`Error writing document: ${err}`);
+      });
+  }
+
+  loadNextImage() {
+    this.setState({
+      regions: [],
+      image: this.state.images[
+        Math.floor(Math.random() * this.state.images.length)
+      ]
+    });
   }
 
   removeRegion(index) {
@@ -23,33 +85,40 @@ class App extends Component {
   }
 
   regionRenderer(regionProps) {
-		return (
-			<div className="remove" onClick={() => this.removeRegion(regionProps.index)}>
+    return (
+      <div
+        className="remove"
+        onClick={() => this.removeRegion(regionProps.index)}
+      >
         X
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 
   onChange(regions) {
-    this.setState({ regions })
+    this.setState({ regions });
   }
 
   render() {
     return (
       <div className="App">
-        <div class="header">
-          Tagger
+        <div className="header">
+          Tagger{!!this.state.image && ` (${this.state.image})`}
         </div>
-        <RegionSelect
-          constraint={true}
-          regions={this.state.regions}
-          onChange={this.onChange}
-          regionRenderer={this.regionRenderer}>
-          <img
-            alt='bucket'
-            width='600px'
-            src={`${repository}${this.state.image}`} />
-        </RegionSelect>
+        {!!this.state.image && (
+          <RegionSelect
+            constraint={true}
+            regions={this.state.regions}
+            onChange={this.onChange}
+            regionRenderer={this.regionRenderer}
+          >
+            <img
+              alt="bucket"
+              width="720px"
+              src={`${repository}${this.state.image}`}
+            />
+          </RegionSelect>
+        )}
       </div>
     );
   }
